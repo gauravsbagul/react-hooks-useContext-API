ab331bcfe27b811c96dfae1db5b6fb0268a94064
Gaurav Bagul <gauravsbagul@gmail.com>
2020-07-26 17:26:06 +0530

    Consumer

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,20 +4,27 @@ import { ThemeContext } from './../contexts/ThemeContext';
 
 class NavBar extends Component {
 
-    static contextType=ThemeContext;
-    render() {
-        const { isLightTheme, dark, light }=this.context;
-        const theme = isLightTheme ? light : dark;
-        return (
-            <nav style={{background:theme.ui, color: theme.syntax}}>
-                <h1>Context App</h1>
-                <ul>
-                    <li>Home</li>
-                    <li>About</li>
-                    <li>Context</li>
 
-                </ul>
-            </nav>
+    render() {
+
+
+        return (
+            <ThemeContext.Consumer>{(context) => {
+                const { isLightTheme, dark, light } = context;
+                const theme=isLightTheme? light:dark;
+                return (
+                    <nav style={{ background: theme.ui, color: theme.syntax }}>
+                        <h1>Context App</h1>
+                        <ul>
+                            <li>Home</li>
+                            <li>About</li>
+                            <li>Context</li>
+                        </ul>
+                    </nav>
+
+                )
+            }}
+            </ThemeContext.Consumer>
         );
     }
 }
